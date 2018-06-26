8488a18885f148e1ed31e446b1fafaed0e7b455f
HaHaJeff <327767852@qq.com>
2018-06-26 21:43:30 +0800

    leetcode bigdata

--- leetcode/bigData/multi.cc
+++ leetcode/bigData/multi.cc
@@ -1,9 +1,22 @@
 #include <iostream>
 
+inline int GetLeft(int a, int n) {
+
+}
+
+inline int GetRight(int b, int m) {
+
+}
+
 //a*b = (2^n/2 * a_left + a_right)*(2^n/2 * b_left + b_right);
 // ==> 2^n*a_left*b_left + a_right*b_right + 2^n/2 * a_left * b_tight
 // +2^n/2 * a_rifht * b_left
 // a_left*b_right + a_right*b_left = (a_left - a_right)(b_right - b_left)
-int Recur(int a, int b) {
+int Recur(int a, int b, int n, int m) {
+
+  int a_left = GetLeft(a, n);
+  int a_right = GetRight(a, n);
+  int b_left = GetLeft(b, m);
+  int b_right = GetRight(b, m);
 
 }
