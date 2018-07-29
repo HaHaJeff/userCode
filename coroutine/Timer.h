4b4e426b7cf782461e5c75a70ea5d8da1b245567
MSGF-NGINX <327767852@qq.com>
2018-07-29 18:14:08 +0800

    coroutine add time.h

---
+++ coroutine/Timer.h
@@ -0,0 +1,41 @@
+#pragma once
+
+#include <cinttypes>
+#include <cstdlib>
+#include <vector>
+
+typedef struct CoContextSocket CoContextSocket_t;
+class Timer {
+public:
+    Timer();
+    ~Timer();
+
+    void AddTimer(uint64_t abs_time, CoContextSocket_t *socket);
+    void RemoveTImer(const size_t timer_id);
+    CoContextSocket_t* PopTimeout();
+    const int GetNextTimeout() const;
+    const bool IsEmpty();
+    static const uint64_t GetTimestampMS();
+    static const uint64_t GetSteadyClockMS();
+    static void MsSleep(const int time_ms);
+    std::vector<CoContextSocket_t *> GetSocketList();
+
+private:
+    void heap_up(const size_t end_idx);
+    void heap_down(const size_t begin_idx);
+
+    struct TimerObj {
+        TimerObj(uint64_t abs_time, CoContextSocket_t *socket)
+            : abs_time(abs_time), socket_(socket) {
+
+            }
+        uint64_t abs_time;
+        CoContextSocket_t *socket_;
+
+        bool operator ==(const TimerObj& obj) const {
+            return abs_time == obj.abs_time_;
+        }
+    };
+
+    std::vector<TimeObj> timer_heap_;
+};
