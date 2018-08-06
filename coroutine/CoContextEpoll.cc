01a9abedfcdb92ab5c7b32d376d3dff18e6bd193
HaHaJeff <327767852@qq.com>
2018-08-06 11:32:54 +0800

    coroutine modify CoContextEpoll

--- coroutine/CoContextEpoll.cc
+++ coroutine/CoContextEpoll.cc
@@ -1,16 +1,76 @@
 #include "CoContextEpoll.h"
+
 #include <cstdlib>
-typedef struct CoContextSocket {
+#include <unistd.h>
+#include <fcntl.h>
+
+namespace coroutine
+{
+typedef struct CoContextSocket
+{
+    EpollScheduler *scheduler;
+    int coroutine_id;
+
+    int epoll_fd;
+
+    int socket;
+    int connect_timeout_ms;
+    int socket_timeout_ms;
+
+    int waited_events;
     size_t timer_id;
+    struct epoll_event event;
+    void *args;
 } CoContextSocket_t;
 
-void CoContextSocketSetTimerID(CoContextSocket_t &socket, size_t timer_id) {
+EpollNotify::EpollNotify(EpollScheduler *scheduler) : scheduler_(scheduler)
+{
+    pipe_fds_[0] = pipe_fds_[1] = -1;
+}
+
+EpollNotify::~EpollNotify()
+{
+    if (pipe_fds_[0] != -1)
+    {
+        close(pipe_fds_[0]);
+    }
+    if (pipe_fds_[1] != -1)
+    {
+        close(pipe_fds_[1]);
+    }
+}
+
+//将Func作为一个协程运行
+void EpollNotifier::Run()
+{
+    fcntl(pipe_fds_[1], F_SETFL, O_NONBLOCK);
+    scheduler_->AddTask(std::bind(&EpollNotifier::Func, this), nullptr)
+}
+
+void EpollNotifier::Func()
+{
+    CoContextSocket_t *socket{scheduler_->CreateSocket(pipe_fds_[0], -1, -1, false)};
+    char tmp[2] = {0};
+    while (true)
+    {
+        if (CoContextRead(*socket, tmp, 1, 0) < 0)
+        {
+            break;
+        }
+    }
+    free(socket);
+}
+
+void CoContextSocketSetTimerID(CoContextSocket_t &socket, size_t timer_id)
+{
     socket.timer_id = timer_id;
 }
 
 //memory should free by user
-CoContextSocket_t* NewCoContextSocket() {
-  //allocate an array, and set memory to zero
-  CoContextSocket_t *socket = (CoContextSocket_t*)calloc(1, sizeof(CoContextSocket_t));
-  return socket;
+CoContextSocket_t *NewCoContextSocket()
+{
+    //allocate an array, and set memory to zero
+    CoContextSocket_t *socket = (CoContextSocket_t *)calloc(1, sizeof(CoContextSocket_t));
+    return socket;
 }
+} // namespace coroutine
--- coroutine/CoContextEpoll.h
+++ coroutine/CoContextEpoll.h
@@ -1,8 +1,91 @@
 #pragma once
+
 #include <cstddef>
+#include <queue>
+
+#include "CoContextRuntime.h"
+#include "timer.h"
+
+namespace coroutine
+{
+
+class EpollScheduler;
 
 typedef struct CoContextSocket CoContextSocket_t;
+typedef std::pair<EpollScheduler *, int> EpollArgs_t;
+typedef std::function<CoContextSocket_t *()> CoContextActiveSocket_t;
+typedef std::function<void()> CoContextHandlerAcceptedFdFunc_t;
+typedef std::function<void()> CoContextHandlerNewRequest_t;
 
 void CoContextSocketSetTimerID(CoContextSocket_t &socket, size_t timer_id);
 
-CoContextSocket_t* NewCoContextSocket();
+CoContextSocket_t *NewCoContextSocket();
+
+//EpollScheduler run forever, EpollNotify is used to close EpollScheduler
+//通过pipe_fd机制来结束Run的while(true)
+class EpollNotifier final
+{
+public:
+  EpollNotifier(EpollScheduler *scheduler);
+  ~EpollNotifier();
+
+  void Run();
+  void Func();
+  void Notify();
+
+private:
+  EpollScheduler *scheduler_{nullptr};
+  int pipe_fds_[2];
+};
+
+class EpollScheduler final
+{
+public:
+  EpollScheduler(size_t stack_size, int max_task, const bool need_stack_protect = true);
+  ~EpollScheduler();
+
+  static EpollScheduler *Instance();
+  bool IsTaskFull();
+  void AddTask(CoFunc_t func, void *args);
+  CoContextSocket_t *CreateSocket(const int fd, const int socket_timeout_ms = 5000, const int connect_timeout_ms = 200, const bool no_delay = true);
+  void SetActiveSocketFunc(CoContextActiveSocket_t active_socket_func);
+  void SetHandlerAcceptedFdFunc(CoContextHandlerAcceptedFdFunc_t handler_accepted_fd_func);
+  void SetHandlerNewRequestFunc(CoContextHandlerNewRequest_t handler_new_request_func);
+  bool YiedlTask();
+  bool Run();
+  void Close();
+  void NotifyEpoll();
+  int GetCurrentCoroutine();
+
+  void AddTimer(CoContextSocket_t *socket, const int timeout_ms);
+  void RemoveTimer(const size_t timer_id);
+  void DealwithTimeout(int &next_timeout);
+
+private:
+  typedef std::queue<std::pair<CoFunc_t, void *>> TaskQueue_t;
+
+  void ConsumeTodoList();
+  void ResumeAll(int flag);
+  void StatEpollwaitEvents(const int event_count);
+
+  CoContextRuntime runtime_;
+  int max_task_;
+  TaskQueue_t to_list_;
+  int epoll_fd_;
+
+  Timer timer_;
+  bool closed_{false};
+  bool run_forever_{false};
+
+  CoContextActiveSocket_t active_socket_func_;
+  CoContextHandlerAcceptedFdFunc_t handler_accepted_fd_func_;
+  CoContextHandlerNewRequest_t handler_new_request_func_;
+
+  int epoll_wait_events_;
+  int epoll_wait_events_per_second_;
+  uint64_t epoll_wait_events_last_cal_time_;
+  t
+
+      EpollNotifier epoll_wake_up_;
+};
+} // namespace coroutine
