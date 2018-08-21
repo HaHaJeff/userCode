da222836d46fd6056a65ab8bf6acd643a3a6815f
HaHaJeff <327767852@qq.com>
2018-08-21 10:37:02 +0800

    leetcode triangle eliminate if

--- leetcode/triangle/main.cc
+++ leetcode/triangle/main.cc
@@ -11,15 +11,7 @@ public:
 		//dp[i][j] = min(dp[i-1][j], dp[i-1][j-1]) + triangles[i][j]
 		for (int i = 1; i < triangle.size(); i++) {
 			for (int j = 0; j < i + 1; j++) {
-				if (j == 0) {
-					triangle[i][j] += triangle[i - 1][j];
-				}
-				else if (j == i) {
-					triangle[i][j] += triangle[i - 1][j - 1];
-				}
-				else {
-					triangle[i][j] += min(triangle[i - 1][j - 1], triangle[i - 1][j]);
-				}
+                triangle[i][j] += (j == 0 ? triangle[i-1][j] : (j == i ? triangle[i-1][j-1] : (min(triangle[i-1][j-1], triangle[i-1][j]))));
 			}
 		}
 
