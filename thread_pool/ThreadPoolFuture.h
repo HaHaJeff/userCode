51452670d9fdb95f9f72b073b1fe6a47e5edeb46
HaHaJeff <327767852@qq.com>
2018-08-24 10:44:53 +0800

    thread_pool simple future

--- thread_pool/ThreadPoolFuture.h
+++ thread_pool/ThreadPoolFuture.h
@@ -8,14 +8,15 @@
 #include <functional>
 #include <atomic>
 #include <queue>
-
+#include <mutex>
+#include <future>
 
 class ThreadPool {
 public:
-  explicit ThredadPool(std::size_t threads = std::max(4u, std::thread::hardware_concurrency() * 2));
+  explicit ThreadPool(std::size_t threads = std::max(4u, std::thread::hardware_concurrency() * 2));
 
   template <class Func, class ...Args>
-  auto AddTask(Func&& f, Args&&... args) -> typename std::result_of<Func(Args...)>::type;
+  auto AddTask(Func&& f, Args&&... args) -> std::future<typename std::result_of<Func(Args...)>::type>;
 
   ~ThreadPool();
 
@@ -25,24 +26,69 @@ private:
   std::queue<std::function<void()>> tasks_;
   std::size_t max_queue_size_ {100000};
   bool stop_{false};
+
+
+  std::condition_variable condition_;
+  std::mutex mutex_;
 };
 
+ThreadPool::ThreadPool(std::size_t threads) {
+  for (size_t i = 0; i < threads; ++i) {
+    workers_.emplace_back(
+        [this]
+        {
+        while(true) {
+          std::function<void()> task;
+          {
+            std::unique_lock<std::mutex> lock(this->mutex_);
+            condition_.wait(lock, [this]{return this->stop_ || !tasks_.empty();});
+
+            if (stop_ && tasks_.empty()) {
+              return;
+            }
+            else if (!this->tasks_.empty()) {
+              task = std::move(this->tasks_.front());
+              this->tasks_.pop();
+            }
+            else {
+              continue;
+            }
+
+          }
+          task();
+        }
+        }
+        );
+  }
+}
+
 template <class Func, class ...Args>
-auto ThreadPool::AddTask(Func&& f, Args&&... args)
-  -> std::future<typename std::result_of<Func(Args...)>::type>
+auto ThreadPool::AddTask(Func&& f, Args&&... args) -> std::future<typename std::result_of<Func(Args...)>::type>
 {
   using return_type = typename std::result_of<Func(Args...)>::type;
 
   auto task = std::make_shared<std::packaged_task<return_type()>>(std::bind(std::forward<Func>(f), std::forward<Args>(args)...));
 
-  auto result = task.get_future();
+  auto result = task->get_future();
 
   tasks_.emplace([task](){
       (*task)();   //std::function<void()>;
       });
 
-
+  condition_.notify_one();
   return result;
+}
+
+ThreadPool::~ThreadPool() {
+
+  {
+    std::unique_lock<std::mutex> lock(this->mutex_);
+    stop_ = true;
+  }
+
+  for(auto &worker: workers_) {
+    worker.join();
+  }
 }
 
 #endif
