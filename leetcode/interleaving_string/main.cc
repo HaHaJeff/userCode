886ed7098af8cde2cf2bd1a95c92248c64159041
HaHaJeff <327767852@qq.com>
2018-08-27 21:32:32 +0800

    leetcode interleaving_string

---
+++ leetcode/interleaving_string/main.cc
@@ -0,0 +1,37 @@
+#include <string>
+#include <iostream>
+#include <vector>
+using namespace std;
+
+class Solution {
+public:
+	bool isInterleave(string s1, string s2, string s3) {
+		int size1 = s1.length();
+		int size2 = s2.length();
+
+		if (size1 + size2 != s3.size()) return false;
+		vector<vector<bool>> invalid(size1+1, vector<bool>(size2+1, false));
+		return recur(s1, s2, s3, 0, 0, 0, invalid);
+	}
+
+	bool recur(string s1, string s2, string s3, int i, int j, int k, vector<vector<bool>> invalid) {
+		if (invalid[i][j]) return false;
+
+		bool valid = (i < s1.length() && s1[i] == s3[k] && recur(s1, s2, s3, i + 1, j, k + 1, invalid)) ||
+			(j < s2.length() && s2[j] == s3[k] && recur(s1, s2, s3, i, j + 1, k + 1, invalid));
+
+		if (!valid) invalid[i][j] = true;
+
+		return valid;
+	}
+};
+
+int main()
+{
+	string s1 = "aabcc";
+	string s2 = "dbbca";
+	string s3 = "aadbbbaccc";
+
+	Solution sol;
+	std::cout << sol.isInterleave(s1, s2, s3) << std::endl;
+}
