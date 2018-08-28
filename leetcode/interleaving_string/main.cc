5370b5558771296f91e0b05fd3e668b4e7ea1d6c
HaHaJeff <327767852@qq.com>
2018-08-28 20:36:21 +0800

    leetcode interleaving_string add dp

--- leetcode/interleaving_string/main.cc
+++ leetcode/interleaving_string/main.cc
@@ -25,6 +25,30 @@ public:
 
 		return valid;
 	}
+    	//dp[i][j]表示 s1 i位置以及 s2 j位置是否满足 s3 i+j
+	//如果s3满足s1与s2 interleaving，那么s3[last] == s1[last] or s2[last]递推回去
+	bool dp(string s1, string s2, string s3) {
+		int size1 = s1.size();
+		int size2 = s2.size();
+		int size3 = s3.size();
+		vector<vector<bool>> dp(size1+1, vector<bool>(size2+1, false));
+
+		for (int i = 1; i < size1 + 1; i++) {
+			dp[i][0] = s1.substr(0, i) == s3.substr(0, i) ? true : false;
+		}
+
+		for (int j = 1; j < size2 + 1; j++) {
+			dp[0][j] = s2.substr(0, j) == s3.substr(0, j) ? true : false;
+		}
+
+		for (int i = 1; i < size1 + 1; i++) {
+			for (int j = 1; j < size2 + 1; j++) {
+				dp[i][j] = (s1[i - 1] == s3[i - 1 + j] ? dp[i - 1][j] : (s2[j - 1] == s3[i + j - 1] ? dp[i][j - 1] : false));
+			}
+		}
+
+		return dp[size1][size2];
+	}
 };
 
 int main()
