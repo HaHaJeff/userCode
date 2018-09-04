2f9700e67922ac303638e647dc80e1e4d3c8cab8
HaHaJeff <327767852@qq.com>
2018-09-04 19:36:12 +0800

    leetcode modify perfect squares

--- leetcode/perfect_squares/main.cc
+++ leetcode/perfect_squares/main.cc
@@ -1,10 +1,23 @@
 #include <iostream>
 #include <vector>
-
+#include <algorithm>
+#include <map>
 class Solution {
 public:
 	int numSquares(int n) {
-		return recur(n);
+	
+		return dp(n);
+	}
+
+	int dp(int n) {
+		static std::vector<int> dp(1, 0);
+		for (int i = 1; i <= n; i++) {
+			dp[i] = i;
+			for (int j = 1; j*j <= i; j++)  {
+				dp[i] = std::min(dp[i], dp[i - j*j] + 1);
+			}
+		}
+		return dp[n];
 	}
 
 	int recur(int n) {
@@ -14,11 +27,31 @@ public:
 
 		return recur(n - pow(static_cast<int>(sqrt(n)), 2)) + 1;
 	}
+
+	int recur1(int n) {
+
+		if (n == 0) {
+			return 0;
+		}
+
+		int min = INT_MAX;
+		if (dp_.find(n) != dp_.end()) return dp_[n];
+		for (int i = 1; i*i <= n; i++) {
+
+			int ret = recur1(n - i*i) + 1;
+			min = std::min(min, ret);
+
+		}
+		dp_[n] = min;
+		return min;
+	}
+private:
+	std::map<int, int> dp_;
 };
 
 int main()
 {
-	int n = 19;
+	int n = 48;
 
 	Solution sol;
 	std::cout << sol.numSquares(n) << std::endl;
