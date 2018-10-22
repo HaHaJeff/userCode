29752f2196e04184048aaa89b8bd25b2d577f916
HaHaJeff <327767852@qq.com>
2018-10-22 21:42:49 +0800

    net_lib test channel

--- net_lib/net_test/TestTime.cc
+++ net_lib/net_test/TestTime.cc
@@ -1,11 +1,19 @@
 #include <iostream>
+#include <poll.h>
 #include "timestamp.h"
+#include "channel.h"
 #include "net_test.h"
 
 TEST(TestBase, TimeStamp) {
   TimeStamp t(TimeStamp::Now());
   std::cout << t.ToString() << std::endl;
   std::cout << t.ToFormattedString() << std::endl;
+}
+
+TEST(TestBase, Channel) {
+  Channel ch(nullptr, 0);
+  ch.SetREvents(POLLNVAL);
+  ch.HandleEvent();
 }
 
 int main()
