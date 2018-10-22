8eb0fba23ff29c01b2d564933218acbe77b628e4
HaHaJeff <327767852@qq.com>
2018-10-22 21:50:27 +0800

    net_lib test eventloop

--- net_lib/net_test/TestTime.cc
+++ net_lib/net_test/TestTime.cc
@@ -1,5 +1,8 @@
 #include <iostream>
 #include <poll.h>
+#include "eventloop.h"
+#include "log.h"
+#include "timer.h"
 #include "timestamp.h"
 #include "channel.h"
 #include "net_test.h"
@@ -14,6 +17,24 @@ TEST(TestBase, Channel) {
   Channel ch(nullptr, 0);
   ch.SetREvents(POLLNVAL);
   ch.HandleEvent();
+}
+
+TEST(TestBase, EventLoop) {
+  Logger& log = Logger::GetLogger();
+  log.SetFileName("event.log");
+  EventLoop loop;
+
+  int i = 0;
+  while (i < 100) {
+    loop.RunAfter(1.0, [i]
+        {
+          std::cout << "RunAfter: " << i << std::endl;
+        }
+        );
+    i+=1;
+  }
+
+  loop.Loop();
 }
 
 int main()
