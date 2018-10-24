e1e368f0dc9b090798d8d72777d5593b0f2609c9
MSGF-NGINX <327767852@qq.com>
2018-10-24 22:41:02 +0800

    leetcode add letter_combinations_of_a_phone_number

---
+++ leetcode/letter_combinations_of_a_phone_number/main.cc
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    vector<string> letterCombinations(string digits) {
+        vector<string> alp = {"", "", "abc", "def", "ghi", "jkl", "mno", "pqrs", "tuv", "wxyz"};
+        vector<string> results;
+        if (digits.empty()) return results;
+        
+        results.push_back("");
+        for (auto cnum : digits) {
+            int index = cnum - '0';
+            
+            if (alp[index].empty()) continue;
+            
+            vector<string> tmp;
+            for (auto ch : alp[index]) {
+                for (auto c : results) {
+                    tmp.push_back(c+ch);
+                }
+            }
+            
+            results.swap(tmp);
+        }
+        
+        return results;
+    }
+};
