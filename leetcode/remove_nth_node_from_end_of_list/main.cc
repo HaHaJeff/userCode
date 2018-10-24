78ce84fce75992bb67c13400f603d9f3ca84bc23
MSGF-NGINX <327767852@qq.com>
2018-10-24 22:21:38 +0800

    leetcode add remove_nth_node_from_end_of_list

---
+++ leetcode/remove_nth_node_from_end_of_list/main.cc
@@ -0,0 +1,31 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode(int x) : val(x), next(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode* removeNthFromEnd(ListNode* head, int n) {
+        
+        ListNode* fast = head;
+        ListNode** slow = &head;
+        while (--n > 0) {
+            fast = fast->next;
+        }
+        
+        if (fast == nullptr) return head->next;
+        
+        while (fast != nullptr && fast->next != nullptr) {
+            fast = fast->next;
+            slow = &(*slow)->next;
+        }
+        
+        *slow = (*slow)->next;
+        
+        return head;    
+        
+    }
+};
