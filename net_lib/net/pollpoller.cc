191223249555c5a7f868d5caf0b1d596a2a145f7
HaHaJeff <327767852@qq.com>
2018-10-30 21:35:25 +0800

    poller

--- net_lib/net/pollpoller.cc
+++ net_lib/net/pollpoller.cc
@@ -1,16 +1,24 @@
 #include <poll.h>
 #include <errno.h>
+#include <assert.h>
 #include "pollpoller.h"
 #include "log.h"
 #include "channel.h"
+
+/*
+   struct pollfd {
+      int   fd;         // file descriptor
+      short events;     // requested events
+      short revents;    // returned events
+   };
+*/
+
 
 PollPoller::PollPoller(EventLoop* loop) : Poller(loop) {
 }
 
 PollPoller::~PollPoller() {
 }
-
-
 
 TimeStamp PollPoller::Poll(int timeoutMs, ChannelList* activeChannels) {
   int numEvents = poll(&*pollfds_.begin(), pollfds_.size(), timeoutMs);
@@ -26,26 +34,45 @@ TimeStamp PollPoller::Poll(int timeoutMs, ChannelList* activeChannels) {
 }
 
 void PollPoller::FillActiveChannels(int numEvents, ChannelList* activeChannels) const {
-  for (PollFdList::const_iterator pfd = pollfds_.begin();
-      pfd != pollfds_.end() && numEvents > 0; ++pfd) {
+  for (PollFdList::const_iterator pfd = pollfds_.begin(); pfd != pollfds_.end() && numEvents > 0; ++pfd) {
     if (pfd->revents > 0) {
       --numEvents;
       ChannelMap::const_iterator ch = channels_.find(pfd->fd);
       Channel* channel = ch->second;
-      channel->SetREvents(pfd->events);
+      channel->SetREvents(pfd->revents);
       activeChannels->push_back(channel);
     }
   }
 }
 
 void PollPoller::UpdateChannel(Channel* channel) {
-
+  Poller::AssertInLoopThread();
+  TRACE("updating channel %lld fd %d events %d", (long long)channel->GetId(), channel->GetFd(), channel->GetEvents());
+  int fd = channel->GetFd();
+  assert(channels_.find(fd) != channels_.end());
+  struct pollfd pfd;
+  pfd.fd = fd;
+  pfd.events = static_cast<short>(channel->GetEvents());
+  pfd.revents = 0;
+  // FIXME: ignore channel->IsNoneEvent()
 }
 
 void PollPoller::AddChannel(Channel* channel) {
+  Poller::AssertInLoopThread();
+  TRACE("adding channel %lld, fd=%d, events=%d", channel->GetFd(), channel->GetEvents());
+  struct pollfd pfd;
+  pfd.fd = channel->GetFd();
+  pfd.events = channel->GetEvents();
+  pfd.revents = 0;
+  pollfds_.push_back(pfd);
 
+  channels_[pfd.fd] = channel;
 }
 
 void PollPoller::RemoveChannel(Channel* channel) {
-
+  Poller::AssertInLoopThread();
+  TRACE("remove channel %lld fd %d events %d", (long long)channel->GetId(), channel->GetFd(), channel->GetEvents());
+  assert(channels_.find(channel->GetFd()) != channels_.end());
+  assert(channels_[channel->GetFd()] == channel);
+  const struct pollfd& pfd = *pollfds_.find(channel);
 }
