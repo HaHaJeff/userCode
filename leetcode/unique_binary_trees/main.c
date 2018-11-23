be8ea7fefda504c3b63bfff5cbc40bb75ffc0431
HaHaJeff <327767852@qq.com>
2018-11-23 18:56:04 +0800

    leetcode update unique_binary_trees

--- leetcode/unique_binary_trees/main.c
+++ leetcode/unique_binary_trees/main.c
@@ -1,22 +1,23 @@
+#include <iostream>
 class Solution {
 public:
-    int numTrees(int n) {
-        return recur(1, n, true);
-    }
+	int numTrees(int n) {
+		return recur(1, n, true);
+	}
 
-    int recur(int start, int end, bool bLeft) {
-        
-        if (start == end) return 1;
-        
-        int left = 0;
-        int right = 0;
-        int sum = 0;
-        for (int i = start; i <= end; i++) {
-            left += recur(start, i-1, true);
-            right += recur(i+1, end, false);
-            sum += (left*right);
-        }
-        
-        return sum;
-    }
+	int recur(int start, int end, bool bLeft) {
+
+		if (start >= end) return 1;
+
+		int left = 0;
+		int right = 0;
+		int sum = 0;
+		for (int i = start; i <= end; i++) {
+			left = recur(start, i - 1, true);
+			right = recur(i + 1, end, false);
+			sum += (left*right);
+		}
+
+		return sum;
+	}
 };
