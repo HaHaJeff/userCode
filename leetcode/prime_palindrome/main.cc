0c0838cd3b01aab9cf5ee6006b9189755bc316a1
HaHaJeff <327767852@qq.com>
2018-11-29 15:52:50 +0800

    update prime_palindrome

--- leetcode/prime_palindrome/main.cc
+++ leetcode/prime_palindrome/main.cc
@@ -1,6 +1,8 @@
 #include <iostream>
 #include <algorithm>
-class Solution {
+
+// 超时
+class Solution1 {
 public:
 	int primePalindrome(int N) {
 		for (int i = N;; i++) {
@@ -52,8 +54,49 @@ public:
 	}
 };
 
+// 1316ms
+class Solution {
+public:
+	int primePalindrome(int N) {
+		if (1e7 <= N && N <= 1e8)
+			N = 100030001;
+		while (1){
+			while (!isPalindrome(N)) N++;
+			if (isPrime(N)) {
+				return N;
+			}
+			N++;
+		}
+	}
+
+	int getReverse(int N) {
+		int ret = 0;
+
+		while (N) {
+			ret = ret * 10 + N % 10;
+			N /= 10;
+		}
+
+		return ret;
+	}
+
+	bool isPrime(int N) {
+		if (N < 2 || N % 2 == 0) return N == 2;
+		for (int d = 3; (d*d) < (N + 1); d += 2) {
+			if (!(N%d))
+				return false;
+		}
+		return true;
+	}
+
+	bool isPalindrome(int N) {
+		return N == getReverse(N);
+	}
+
+};
+
 int main()
 {
 	Solution sol;
-	std::cout << sol.primePalindrome(9989900) << std::endl;
+	std::cout << sol.primePalindrome(51633903) << std::endl;
 }
