a7849be992c28a82b278fd589dc75e475c49051a
HaHaJeff <327767852@qq.com>
2018-12-05 09:40:06 +0800

    update leetcode contains_duplicate_III

--- leetcode/contains_duplicate_III/main.cc
+++ leetcode/contains_duplicate_III/main.cc
@@ -1,27 +1,29 @@
-#include <iostream>
-#include <vector>
-using namespace std;
-
 class Solution {
 public:
+	bool help(const std::set<long long>& window, long long val, long long target) {
+		// numx[i] - x <= t ====> x >= nums[i] - t
+		auto pos = window.lower_bound(val - target);
+		// nums[i] - x >= -t ===> x - nums[i] <= t
+		if (pos != window.end() && *pos - val <= target) return true;
+		return false;
+	}
+
 	bool containsNearbyAlmostDuplicate(vector<int>& nums, int k, int t) {
-		for (int i = 0; i < nums.size(); i++) {
-			for (int j = i + 1; j < nums.size(); j++) {
-				long long ik = abs((long long)j - i);
-				long long it = abs((long long)nums[j] - nums[i]);
-				if (it <= (long long)t && ik <= (long long)k) {
-					return true;
-				}
+		if (k == 0) return false;
+		std::set<long long> windows;
+		int size = nums.size();
+		long long lk = static_cast<long long>(k);
+		long long lt = static_cast<long long>(t);
+
+		for (int i = 0; i < size; ++i) {
+			if (i > k) windows.erase(nums[i - k - 1]);
+			// |nums[i] - x| <= t  =====>  nums[i] - x >= -t && nums[i] - x <= t
+			if (help(windows, static_cast<long long>(nums[i]), lt)) {
+				return true;
 			}
+			windows.insert(nums[i]);
 		}
 		return false;
 	}
 };
 
-int main()
-{
-	vector<int>nums = { -1, INT_MAX };
-	Solution sol;
-
-	std::cout << sol.containsNearbyAlmostDuplicate(nums, 1, INT_MAX) << std::endl;
-}s
