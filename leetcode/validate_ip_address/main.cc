1daf0591fbf2b8e5b94aa9867b9783d5f5824986
HaHaJeff <327767852@qq.com>
2018-12-06 21:14:07 +0800

    leetcode add validate_ip_address

--- leetcode/validate_ip_address/main.cc
+++ leetcode/validate_ip_address/main.cc
@@ -7,7 +7,7 @@ using namespace std;
 class Solution {
 public:
 	string validIPAddress(string IP) {
-		transform(IP.begin(), IP.end(), IP.begin, ::tolower);
+		transform(IP.begin(), IP.end(), IP.begin(), ::tolower);
 		if (IP.find(":") != string::npos) {
 			return ipv6(IP) ? "IPv6" : "Neither";
 		}
@@ -30,7 +30,7 @@ public:
 
 				string sub = string(it, it2);
 
-				if (sub.size() < 1 || sub.size() > 3 || sub[0] == '0' || stoi(sub) > 255) {
+				if (sub.size() < 1 || sub.size() > 3 || (sub[0] == '0' && sub.size() > 1) || stoi(sub) > 255) {
 					return false;
 				}
 
@@ -44,7 +44,7 @@ public:
 					return false;
 				}
 				it++;
-				if (it != IP.end() && (*it) != '.') {
+				if (it != IP.end() && (*it) == '.') {
 					return false;
 				}
 			}
@@ -53,11 +53,51 @@ public:
 	}
 
 	bool ipv6(string IP) {
-		return false;
+		
+		int cnt = 0;
+		function < bool(char)> isHex = [](char c){return isdigit(c) || (c <= 'f' && c >= 'a'); };
+		for (auto it = IP.begin(); it != IP.end();) {
+			if (isHex(*it)) {
+				auto it2 = find_if_not(it, IP.end(), isHex);
+
+				if (it2 != IP.end() && *it2 != ':') {
+					return false;
+				}
+
+				string sub = string(it, it2);
+
+				if (sub.size() > 4) {
+					return false;
+				}
+
+				++cnt;
+
+				if (cnt == 8 && it2 != IP.end()) {
+					return false;
+				}
+
+				it = it2;
+			}
+			else {
+				if (*it != ':') {
+					return false;
+				}
+				++it;
+				if (it != IP.end() && *it == ':') {
+					return false;
+				}
+			}
+		}
+
+
+		return cnt == 8;
 	}
 };
 
 int main()
 {
+	std::string ipv4 = "192.0.0.1";
 
+	Solution sol;
+	std::cout << sol.validIPAddress(ipv4) << std::endl;
 }
