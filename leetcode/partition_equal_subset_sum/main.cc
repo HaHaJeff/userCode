1904bea016e1aa22446d1a36239c01b35606a0aa
HaHaJeff <327767852@qq.com>
2018-12-09 21:43:47 +0800

    leetcode update partition_equal_subset_sum dp

--- leetcode/partition_equal_subset_sum/main.cc
+++ leetcode/partition_equal_subset_sum/main.cc
@@ -15,17 +15,47 @@ public:
 
 		return false;
 	}
-	bool canPartition(vector<int>& nums) {
+
+	//0-1背包选或不选
+	//dp[i][j] = dp[i-1][j-nums[i]](选)   dp[i][j] = dp[i-1][j](不选)
+	bool dp(vector<int>& nums, int start, int target) {
+		vector<vector<int>> dp(nums.size()+1, vector<int>());
+		for (auto& item : dp) {
+			item.resize(target + 1);
+		}
 		
+		dp[0][0] = true;
+
+		for (int i = 1; i <= nums.size(); ++i) {
+			dp[i][0] = true;
+		}
+
+		for (int j = 1; j <= target; ++j) {
+			dp[0][j] = false;
+		}
+
+		for (int i = 1; i <= nums.size(); ++i) {
+			for (int j = 1; j <= target; ++j) {
+				
+				dp[i][j] = dp[i - 1][j];
+				if (j >= nums[i-1])
+					dp[i][j]  = dp[i][j] || dp[i - 1][j - nums[i-1]];
+			}
+		}
+
+		return dp[nums.size()][target];
+	}
+
+	bool canPartition(vector<int>& nums) {	
 		int sums = accumulate(nums.begin(), nums.end(), 0);
 		if (sums & 1) return false;
-		return backTrack(nums, 0, sums / 2);
+		return dp(nums, 0, sums / 2);
 	}
 };
 
 int main()
 {
 	Solution sol;
-	std::vector<int> nums = { 1, 2, 3, 5 };
+	std::vector<int> nums = { 1, 3, 3, 5 };
 	std::cout << sol.canPartition(nums) << std::endl;
 }
