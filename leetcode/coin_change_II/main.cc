c10c926b310d33e7f20e9309d929a30a4e3eec85
HaHaJeff <327767852@qq.com>
2018-12-12 17:39:50 +0800

    leetcode accepted coin_change_II

--- leetcode/coin_change_II/main.cc
+++ leetcode/coin_change_II/main.cc
@@ -5,6 +5,10 @@ using namespace std;
 class Solution {
 public:
 	int change(int amount, vector<int>& coins) {
+		dp.resize(amount + 1);
+		for (auto& item : dp) {
+			item.resize(coins.size(), -1);
+		}
 		return recur(amount, 0, coins);
 	}
 
@@ -14,13 +18,17 @@ public:
 			return target == 0 ? 1 : 0;
 		}
 		else {
+			if (dp[target][index] != -1) { return dp[target][index]; }
 			for (int i = 0; coins[index]*i <= target; i++) {
 				cnt += recur(target - i*coins[index], index + 1, coins);
 			}
+			dp[target][index] = cnt;
 		}
 		return cnt;
 
 	}
+private:
+	vector<vector<int>> dp;
 };
 
 int main()
