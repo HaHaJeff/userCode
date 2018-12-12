249e5e70138a905bfc10638893ea3203ffa8faba
HaHaJeff <327767852@qq.com>
2018-12-12 22:06:05 +0800

    leetcode update coin_change_II dp optimize space

--- leetcode/coin_change_II/main.cc
+++ leetcode/coin_change_II/main.cc
@@ -9,7 +9,7 @@ public:
 		for (auto& item : dp) {
 			item.resize(amount + 1, -1);
 		}
-		return recur(amount, 0, coins);
+		return dpOptSpace(amount, coins);
 	}
 
 	int recur(int target, int index, const vector<int>& coins) {
@@ -54,6 +54,59 @@ public:
 
 		return dp[coins.size()-1][target];
 	}
+
+	// dp[i][j] = dp[i-1][j] + dp[i-1][j-arr[i]] + dp[i-1][j-2*arr[i]] + ... + dp[i-1][j-k*arr[i]]
+	// dp[i-1][j-arr[i]] + dp[i-1][j-2*arr[i]] + ... + dp[i-1][j-k*arr[i]] = dp[i][j-arr[i]]
+	// dp[i][j] = dp[i-1][j] + dp[i][j-arr[i]
+	int dpOptSpace(int target, const vector<int>& coins) {
+		if (target == 0) return 1;
+		if (target < 0 || coins.empty()) {
+			return 0;
+		}
+
+		vector<int> dpSpace(target + 1, 0);
+
+		for (int i = 0; coins[0] * i <= target; i++){
+			dpSpace[coins[0]*i] = 1;
+		}
+
+		int num = 0;
+		for (int i = 1; i < coins.size(); i++) {
+			for (int j = 1; j <= target; j++) {
+				dpSpace[j] += (j - coins[i]) >= 0 ? dpSpace[j - coins[i]] : 0;
+			}
+		}
+
+		return dpSpace[target];
+	}
+
+	// dp[i][j] = dp[i-1][j] + dp[i-1][j-arr[i]] + dp[i-1][j-2*arr[i]] + ... + dp[i-1][j-k*arr[i]]
+	// dp[i-1][j-arr[i]] + dp[i-1][j-2*arr[i]] + ... + dp[i-1][j-k*arr[i]] = dp[i][j-arr[i]]
+	// dp[i][j] = dp[i-1][j] + dp[i][j-arr[i]
+	int dpOpt(int target, const vector<int>& coins) {
+
+		if (target < 0 || coins.empty()) {
+			return 0;
+		}
+
+		for (int i = 0; i < coins.size(); i++) {
+			dp[i][0] = 1;
+		}
+
+		for (int j = 0; coins[0] * j <= target; j++){
+			dp[0][coins[0] * j] = 1;
+		}
+
+		int num = 0;
+		for (int i = 1; i < coins.size(); i++) {
+			for (int j = 1; j <= target; j++) {
+				dp[i][j] = dp[i - 1][j];
+				dp[i][j] += (j - coins[i]) >= 0 ? dp[i][j - coins[i]] : 0;
+			}
+		}
+
+		return dp[coins.size() - 1][target];
+	}
 private:
 	vector<vector<int>> dp;
 };
@@ -67,3 +120,4 @@ int main()
 
 	std::cout << sol.change(amount, coins) << std::endl;
 }
+
