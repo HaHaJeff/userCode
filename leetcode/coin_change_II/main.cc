2a70756c2917d7829d0d02446c04ee5982cfc098
HaHaJeff <327767852@qq.com>
2018-12-12 21:09:40 +0800

    leetcode coin_change_II use dp

--- leetcode/coin_change_II/main.cc
+++ leetcode/coin_change_II/main.cc
@@ -5,9 +5,9 @@ using namespace std;
 class Solution {
 public:
 	int change(int amount, vector<int>& coins) {
-		dp.resize(amount + 1);
+		dp.resize(coins.size());
 		for (auto& item : dp) {
-			item.resize(coins.size(), -1);
+			item.resize(amount + 1, -1);
 		}
 		return recur(amount, 0, coins);
 	}
@@ -18,14 +18,41 @@ public:
 			return target == 0 ? 1 : 0;
 		}
 		else {
-			if (dp[target][index] != -1) { return dp[target][index]; }
+			if (dp[index][target] != -1) { return dp[index][target]; }
 			for (int i = 0; coins[index]*i <= target; i++) {
 				cnt += recur(target - i*coins[index], index + 1, coins);
 			}
-			dp[target][index] = cnt;
+			dp[index][target] = cnt;
 		}
 		return cnt;
 
+	}
+
+	int dpOrg(int target, const vector<int>& coins) {
+		
+		if (target < 0 || coins.empty()) {
+			return 0;
+		}
+
+		for (int i = 0; i < coins.size(); i++) {
+			dp[i][0] = 1;
+		}
+
+		for (int j = 0; coins[0] * j <= target; j++){
+			dp[0][coins[0] * j] = 1;
+		}
+
+		int num = 0;
+		for (int i = 1; i < coins.size(); i++) {
+			for (int j = 1; j <= target; j++) {
+				for (int k = 0; j - coins[i] * k >= 0; k++) {
+					num += dp[i - 1][j - coins[i] * k];
+				}
+				dp[i][j] = num;
+			}
+		}
+
+		return dp[coins.size()-1][target];
 	}
 private:
 	vector<vector<int>> dp;
