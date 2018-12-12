13f3f8ba65d255743b9596791bdfc374530217ad
HaHaJeff <327767852@qq.com>
2018-12-12 17:35:18 +0800

    leetcode add coin_change_II force

---
+++ leetcode/coin_change_II/main.cc
@@ -0,0 +1,34 @@
+#include <vector>
+#include <iostream>
+using namespace std;
+
+class Solution {
+public:
+	int change(int amount, vector<int>& coins) {
+		return recur(amount, 0, coins);
+	}
+
+	int recur(int target, int index, const vector<int>& coins) {
+		int cnt = 0;
+		if (index == coins.size()) {
+			return target == 0 ? 1 : 0;
+		}
+		else {
+			for (int i = 0; coins[index]*i <= target; i++) {
+				cnt += recur(target - i*coins[index], index + 1, coins);
+			}
+		}
+		return cnt;
+
+	}
+};
+
+int main()
+{
+	Solution sol;
+
+	vector<int> coins = { 1, 2, 5 };
+	int amount = 5;
+
+	std::cout << sol.change(amount, coins) << std::endl;
+}
