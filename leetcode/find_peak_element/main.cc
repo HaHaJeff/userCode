583b4f0b1d4dc3bccdb99d553ca695e3007efe90
HaHaJeff <327767852@qq.com>
2018-12-16 20:58:25 +0800

    leetcode update find_peak_element

--- leetcode/find_peak_element/main.cc
+++ leetcode/find_peak_element/main.cc
@@ -5,7 +5,7 @@ using namespace std;
 
 class Solution {
 public:
-	int findPeakElement(vector<int>& nums) {
+	int findPeakElementForce(vector<int>& nums) {
 		if (nums.empty() || nums.size() == 1) return 0;
 		if (nums[0] > nums[1]) {
 			return 0;
@@ -17,6 +17,19 @@ public:
 			if (nums[i] > nums[i - 1] && nums[i] > nums[i + 1]) return i;
 		}
 		return 0;
+	}
+	// if mid > mid + 1, untilt to mid < mid + 1
+	int findPeakElement(vector<int>& nums) {
+
+		int low = 0;
+		int high = nums.size() - 1;
+		int mid = (low + high) / 2;
+		while (low < high) {
+			mid = (low + high) / 2;
+			if (nums[mid] < nums[mid + 1]) low = mid + 1;
+			else high = mid;
+		}
+		return low;
 	}
 };
 
