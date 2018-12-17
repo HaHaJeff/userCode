4564c0bba92a0567ad6e7737bc08deeab1abb8d8
HaHaJeff <327767852@qq.com>
2018-12-17 16:55:11 +0800

    leetcode update advantage_shuffle_submissions

--- leetcode/advantage_shuffle_submissions/main.cc
+++ leetcode/advantage_shuffle_submissions/main.cc
@@ -4,24 +4,24 @@
 #include <set>
 using namespace std;
 
+static int x = []() { ios::sync_with_stdio(false); cin.tie(nullptr); return 0; }();
 class Solution {
 public:
 	vector<int> advantageCount(vector<int>& A, vector<int>& B) {
-		
+
 		vector<int> ret;
 		multiset<int> s(A.begin(), A.end());
-		while (!s.empty()) {
-			for (auto n : B) {
-				auto iter = s.upper_bound(n);
-				if (iter == s.end()) {
-					ret.push_back(*s.begin());
-					s.erase(s.begin());
-					continue;
-				}
-				ret.push_back(*iter);
-				s.erase(iter);
+		for (auto n : B) {
+			auto iter = s.upper_bound(n);
+			if (iter == s.end()) {
+				ret.push_back(*s.begin());
+				s.erase(s.begin());
+				continue;
 			}
+			ret.push_back(*iter);
+			s.erase(iter);
 		}
+
 		return ret;
 	}
 };
@@ -29,11 +29,11 @@ public:
 int main()
 {
 	Solution sol;
-	vector<int> A = { 2, 0, 4, 1, 2 }, B = { 1 ,3, 0, 0, 2 };
+	vector<int> A = { 2, 0, 4, 1, 2 }, B = { 1, 3, 0, 0, 2 };
 	vector<int> ret = sol.advantageCount(A, B);
 
 	for (auto i : ret) {
-		std::cout << i << " "; 
+		std::cout << i << " ";
 	}
 	std::cout << std::endl;
 }
