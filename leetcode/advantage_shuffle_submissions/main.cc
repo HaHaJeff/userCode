a49d1240a5c91e7f779b8d7aed57f753257067fe
HaHaJeff <327767852@qq.com>
2018-12-17 16:37:55 +0800

    leetcode add advantage_shuffle_submissions

---
+++ leetcode/advantage_shuffle_submissions/main.cc
@@ -0,0 +1,39 @@
+#include <vector>
+#include <iostream>
+#include <algorithm>
+#include <set>
+using namespace std;
+
+class Solution {
+public:
+	vector<int> advantageCount(vector<int>& A, vector<int>& B) {
+		
+		vector<int> ret;
+		multiset<int> s(A.begin(), A.end());
+		while (!s.empty()) {
+			for (auto n : B) {
+				auto iter = s.upper_bound(n);
+				if (iter == s.end()) {
+					ret.push_back(*s.begin());
+					s.erase(s.begin());
+					continue;
+				}
+				ret.push_back(*iter);
+				s.erase(iter);
+			}
+		}
+		return ret;
+	}
+};
+
+int main()
+{
+	Solution sol;
+	vector<int> A = { 2, 0, 4, 1, 2 }, B = { 1 ,3, 0, 0, 2 };
+	vector<int> ret = sol.advantageCount(A, B);
+
+	for (auto i : ret) {
+		std::cout << i << " "; 
+	}
+	std::cout << std::endl;
+}
