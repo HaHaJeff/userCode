022662043fe6db5907e41850c32ce8fc4be6210c
MSGF-NGINX <327767852@qq.com>
2018-12-20 23:31:59 +0800

    leetcode udate combination_sums

--- leetcode/combination_sums/main.cc
+++ leetcode/combination_sums/main.cc
@@ -4,21 +4,19 @@ public:
         int sum = 0;
         vector<vector<int>> results;
         vector<int> result;
-        backtrack(candidates, target, 0, sum, result, results);
+        std::sort(candidates.begin(), candidates.end());
+        backtrack(candidates, target, 0, result, results);
         return results;
     }
     
-    void backtrack(const vector<int>& candidates, int target, int begin, int sum, vector<int>& result, vector<vector<int>>& results) {
-        if (target < sum) {
-            return;
-        }
-        if (target == sum) {
+    void backtrack(const vector<int>& candidates, int target, int begin, vector<int>& result, vector<vector<int>>& results) {
+        if (target == 0) {
             results.push_back(result);
             return;
         }
-        for (int i = begin; i < candidates.size(); i++) {
+        for (int i = begin; i < candidates.size()&&target >= candidates[i]; i++) {
             result.push_back(candidates[i]);
-            backtrack(candidates, target, i, sum+candidates[i], result, results);
+            backtrack(candidates, target-candidates[i], i,result, results);
             result.pop_back();            
         }
     }
