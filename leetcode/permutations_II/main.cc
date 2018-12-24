18f9eed4c56588922105172f9fc05d75ae38ef62
HaHaJeff <327767852@qq.com>
2018-12-24 22:40:13 +0800

    leetcode add permutations_II

---
+++ leetcode/permutations_II/main.cc
@@ -0,0 +1,47 @@
+class Solution1 {
+public:
+	vector<vector<int>> permuteUnique(vector<int>& nums) {
+
+		vector<vector<int>> results;
+		vector<int> result;
+		std::sort(nums.begin(), nums.end());
+		backtrace(nums, results, 0);
+		return results;
+	}
+
+	void backtrace(vector<int>& nums, vector<vector<int>>& results, int begin) {
+
+		if (begin == nums.size()-1) {
+			results.push_back(nums);
+			return;
+		}
+
+		for (int i = begin; i < nums.size(); i++) {
+			if (i != begin && nums[i] == nums[begin]) continue;
+			swap(nums[begin], nums[i]);
+			backtrace(nums, results, begin + 1);
+		//	swap(nums[begin], nums[i]);
+		}
+	}
+};
+
+class Solution {
+public:
+	void recursion(vector<int> num, int i, int j, vector<vector<int> > &res) {
+		if (i == j - 1) {
+			res.push_back(num);
+			return;
+		}
+		for (int k = i; k < j; k++) {
+			if (i != k && num[i] == num[k]) continue;
+			swap(num[i], num[k]);
+			recursion(num, i + 1, j, res);
+		}
+	}
+	vector<vector<int> > permuteUnique(vector<int> &num) {
+		sort(num.begin(), num.end());
+		vector<vector<int> >res;
+		recursion(num, 0, num.size(), res);
+		return res;
+	}
+};
