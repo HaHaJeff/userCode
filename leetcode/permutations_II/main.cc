d3274adfb124f39f556d9eb7b072ce17b2f176a3
HaHaJeff <327767852@qq.com>
2018-12-25 08:59:18 +0800

    leetcode update permutations_II

--- leetcode/permutations_II/main.cc
+++ leetcode/permutations_II/main.cc
@@ -45,3 +45,32 @@ public:
 		return res;
 	}
 };
+
+class Solution {
+public:
+	void recursion(vector<int>& num, int i, int j, vector<vector<int> > &res) {
+		if (i == j - 1) {
+			res.push_back(num);
+			return;
+		}
+		for (int k = i; k < j; k++) {
+			int z = 0;
+			for (z = i; z < k; z++) {
+				if (z != k && num[z] == num[k]) break;
+			}
+			if (z < k) {
+				continue;
+			}
+			
+			swap(num[i], num[k]);
+			recursion(num, i + 1, j, res);
+			swap(num[i], num[k]);
+		}
+	}
+	vector<vector<int> > permuteUnique(vector<int> &num) {
+		sort(num.begin(), num.end());
+		vector<vector<int> >res;
+		recursion(num, 0, num.size(), res);
+		return res;
+	}
+};
