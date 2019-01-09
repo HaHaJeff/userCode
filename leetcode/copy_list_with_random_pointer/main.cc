21b063f98b796f1340a393afbc75b10713a05653
HaHaJeff <327767852@qq.com>
2019-01-09 22:14:15 +0800

    leetcode update copy_list_with_random_pointer

--- leetcode/copy_list_with_random_pointer/main.cc
+++ leetcode/copy_list_with_random_pointer/main.cc
@@ -1,37 +1,49 @@
 /**
-* Definition for singly-linked list with a random pointer.
-* struct RandomListNode {
-*     int label;
-*     RandomListNode *next, *random;
-*     RandomListNode(int x) : label(x), next(NULL), random(NULL) {}
-* };
-*/
-#include "leetcode.h"
-
+ * Definition for singly-linked list with a random pointer.
+ * struct RandomListNode {
+ *     int label;
+ *     RandomListNode *next, *random;
+ *     RandomListNode(int x) : label(x), next(NULL), random(NULL) {}
+ * };
+ */
 class Solution {
 public:
+
 	RandomListNode *copyRandomList(RandomListNode *head) {
 		if (head == nullptr) {
 			return nullptr;
 		}
-		map<RandomListNode*, RandomListNode*> records;
-		for (auto node = head; node != nullptr;  node = node->next) {
+
+		//利用node的next字段保存当前node的copy
+		//利用copy的next字段保存当前node的next
+		for (auto node = head; node != nullptr;) {
 			auto newNode = new RandomListNode(node->label);
-			//newNode->next = node->next;
-			//newNode->random = node->random;
-			records[node] = newNode;
-		}
-		
-		for (auto node = head; node != nullptr; node = node->next) {
-			//node->random = records[newHead->random];
-			auto newNode = records[node];
-			if (node->next != nullptr)
-				newNode->next = records[node->next];
-			if (node->random != nullptr)
-				newNode->random = records[node->random];
-
+			auto next = node->next;
+			node->next = newNode;
+			newNode->next = next;
+			node = next;
 		}
 
-		return records[head];
+		//改变newNode的random字段
+		for (auto node = head; node != nullptr;) {
+			auto next = node->next->next;
+
+			if (node->random != nullptr) {
+				node->next->random = node->random->next;
+			}
+			node = next;
+		}
+
+		RandomListNode* newHead = new RandomListNode(0);
+		RandomListNode* newNode = newHead;
+		for (auto node = head; node != nullptr;) {
+			
+			auto next = node->next->next;
+			newNode->next = node->next;
+			newNode = node->next;
+            node->next = next;
+			node = next;
+		}
+		return newHead->next;
 	}
 };
