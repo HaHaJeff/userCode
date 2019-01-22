068d570e581f100bbf776885ca1543a3ed17284b
HaHaJeff <327767852@qq.com>
2019-01-22 10:47:23 +0800

    update best_time_to_buy_and_sell_stock_IV

--- leetcode/best_time_to_buy_and_sell_stock_IV/main.cc
+++ leetcode/best_time_to_buy_and_sell_stock_IV/main.cc
@@ -1,39 +1,27 @@
-#include <vector>
-#include <iostream>
-#include <algorithm>
-using namespace std;
-
 class Solution {
 public:
-	int maxProfit(int g, vector<int>& prices) {
-		int maxProfit;
-		vector<vector<int>> dp(g+1, vector<int>(prices.size(), 0));
-
-		// dp[k][i]表示：在第k次第ith天卖出的最大利润；
-		// dp [k][i] = std::max(dp[k][i-1], prices[i] - prices[j] + dp[k-1][j-1]);
-		for (int k = 1; k <= g; k++) {
-			for (int i = 1; i < prices.size(); i++) {
-				int min = prices[0];
-				for (int j = 1; j <= i; j++) {
-					min = std::min(prices[j] - dp[k - 1][j - 1], min);
-				}
-				dp[k][i] = std::max(dp[k][i - 1], prices[i] - min );
-			}
-		}
-
-		for (int i = 1; i <= g; i++) {
-			for (int j = 0; j < prices.size(); j++) {
-				std::cout << dp[i][j] << " ";
-			}
-			std::cout << std::endl;
-		}
-		maxProfit = dp[g][prices.size() - 1];
-		return maxProfit;
-	}
+    
+    // dp[k][i] 第k次选择在第i次卖出的最大利润
+    // dp[k][i] = std::min(dp[k-1][i], prices[i] - prices[j] + dp[k-1][j-1]);
+    // prices[i] - prices[j] + dp[k-1][j-1] = prices[i] - (prices[j] - dp[k-1][j-1])
+    // prices[j] - dp[k-1][j-1]最小即可
+    int maxProfit(int k, vector<int>& prices) {
+        if (prices.empty()) return 0;
+        if (k >= prices.size()/2) {
+            int sum = 0;
+            for (int i = 1; i < prices.size(); i++) {
+                sum += std::max(0, prices[i]-prices[i-1]);
+            }
+            return sum;
+        }
+        vector<int> dp(k+1);
+        vector<int> min(k+1, prices[0]);
+        for (int i = 1; i < prices.size(); i++) {
+            for (int j = 1; j <= k; j++) {
+                min[j] = std::min(min[j], prices[i] - dp[j-1]);
+                dp[j] = std::max(dp[j], prices[i] - min[j]);
+            }
+        }
+        return dp[k];
+    }
 };
-
-int main() {
-	std::vector<int> vec = { 3, 3, 5, 0, 0, 3, 1, 4 };
-	Solution sol;
-	std::cout << sol.maxProfit(2, vec) << std::endl;
-}
