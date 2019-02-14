24ced6889dbf56b5d63bebf53adc6ac2c45ffd07
HaHaJeff <jeffzhouhhh@gmail.com>
2019-02-14 01:16:47 -0800

    leetcode add array_nesting

--- leetcode/array_nesting/main.cc
+++ leetcode/array_nesting/main.cc
@@ -5,8 +5,9 @@ public:
         for (int i = 0; i < nums.size(); i++) {
             int k = i, len = 0;
             while(nums[k] >= 0) {
-                nums[k] = -nums[k];
-                k = -nums[k];
+                int origin = nums[k];
+                nums[k] = -1;
+                k = origin;
                 len++;
             }
             res = max(len, res);
